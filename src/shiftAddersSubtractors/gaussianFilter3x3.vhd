a576b6021fd2bfaaff3ac483b317cba28b57570a
lrafaelz <rafaelmelo.aluno@unipampa.edu.br>
2024-01-03 19:20:10 -0300

    Update data types in tb_gaussianFilter3x3  with shifts (fineshed)

--- src/shiftAddersSubtractors/gaussianFilter3x3.vhd
+++ src/shiftAddersSubtractors/gaussianFilter3x3.vhd
@@ -10,21 +10,32 @@ entity gaussianFilter3x3 is
     port (
         clk     : in std_logic;
         rst     : in std_logic;
-        in_0   : in std_logic_vector(7 downto 0);
-        in_1   : in std_logic_vector(7 downto 0);
-        in_2   : in std_logic_vector(7 downto 0);
-        in_3   : in std_logic_vector(7 downto 0);
-        in_4   : in std_logic_vector(7 downto 0);
-        in_5   : in std_logic_vector(7 downto 0);
-        in_6   : in std_logic_vector(7 downto 0);
-        in_7   : in std_logic_vector(7 downto 0);
-        in_8   : in std_logic_vector(7 downto 0);
+        in_0   : in std_logic_vector(15 downto 0);
+        in_1   : in std_logic_vector(15 downto 0);
+        in_2   : in std_logic_vector(15 downto 0);
+        in_3   : in std_logic_vector(15 downto 0);
+        in_4   : in std_logic_vector(15 downto 0);
+        in_5   : in std_logic_vector(15 downto 0);
+        in_6   : in std_logic_vector(15 downto 0);
+        in_7   : in std_logic_vector(15 downto 0);
+        in_8   : in std_logic_vector(15 downto 0);
         out_val : out std_logic_vector(15 downto 0)
         );
 end entity gaussianFilter3x3;
         
 architecture behavioral of gaussianFilter3x3 is
+    -- registers
+    signal in0_reg              : std_logic_vector(15 downto 0);
+    signal in1_reg              : std_logic_vector(15 downto 0);
+    signal in2_reg              : std_logic_vector(15 downto 0);
+    signal in3_reg              : std_logic_vector(15 downto 0);
+    signal in4_reg              : std_logic_vector(15 downto 0);
+    signal in5_reg              : std_logic_vector(15 downto 0);
+    signal in6_reg              : std_logic_vector(15 downto 0);
+    signal in7_reg              : std_logic_vector(15 downto 0);
+    signal in8_reg              : std_logic_vector(15 downto 0);
     signal out_val_reg          : std_logic_vector(15 downto 0);
+    -- adders
     signal adder_0              : std_logic_vector(15 downto 0);
     signal adder_1              : std_logic_vector(15 downto 0);
     signal adder_2              : std_logic_vector(15 downto 0);
@@ -36,6 +47,7 @@ architecture behavioral of gaussianFilter3x3 is
     signal adder_8              : std_logic_vector(15 downto 0);
     signal adder_9              : std_logic_vector(15 downto 0);
     signal adder_10             : std_logic_vector(15 downto 0);
+    -- subtractors
     signal sub_0                : std_logic_vector(15 downto 0);
     signal sub_1                : std_logic_vector(15 downto 0);
     signal sub_2                : std_logic_vector(15 downto 0);
@@ -56,56 +68,75 @@ architecture behavioral of gaussianFilter3x3 is
         process(clk, rst)
         begin
             if rst = '1' then
-
+                in0_reg <= (others => '0');
+                in1_reg <= (others => '0');
+                in2_reg <= (others => '0');
+                in3_reg <= (others => '0');
+                in4_reg <= (others => '0');
+                in5_reg <= (others => '0');
+                in6_reg <= (others => '0');
+                in7_reg <= (others => '0');
+                in8_reg <= (others => '0');
                 out_val_reg <= (others => '0');
             elsif rising_edge(clk) then
-
+                in0_reg <= in_0;
+                in1_reg <= in_1;
+                in2_reg <= in_2;
+                in3_reg <= in_3;
+                in4_reg <= in_4;
+                in5_reg <= in_5;
+                in6_reg <= in_6;
+                in7_reg <= in_7;
+                in8_reg <= in_8;
                 out_val_reg <= adder_10;
-                end if;
-            end process;
-            -- Line 1
-            adder_0 <= std_logic_vector(resize(std_logic_vector(in_1), in_3'length)) + std_logic_vector(resize(std_logic_vector(in_3), in_1'length));
+            end if;
+        end process;
 
-            adder_1 <= in_5 + in_7;
-            adder_2 <= in_0 + in_2;
-            adder_3 <= in_6 + in_8;
-            --                
-            sll_0 <= std_logic_vector(resize(in_4, sll_0'length) sll 2);
-            sub_0 <= sll_0 - in_4;
+        -- Line 1
+        adder_0 <= in1_reg + in3_reg;
+        adder_1 <= in5_reg + in7_reg;
+        adder_2 <= in0_reg + in2_reg;
+        adder_3 <= in6_reg + in8_reg;
+        --
 
-            -- Line 2
-            adder_4 <= adder_0 + adder_1;
-            adder_5 <= adder_2 + adder_3;
-            --
+        sll_0 <= to_stdlogicvector(to_bitvector(in4_reg) sll 2);
+        sub_0 <= sll_0 - in4_reg;
 
-            sll_1 <= to_stdlogicvector(to_bitvector(adder_4) sll 4);
-            sub_1 <= sll_1 - adder_4;
-            sll_2 <= to_stdlogicvector(to_bitvector(adder_5) sll 4);
-            sub_2 <= sll_2 - adder_5;
+        -- Line 2
+        adder_4 <= adder_0 + adder_1;
+        adder_5 <= adder_2 + adder_3;
+        --
 
-            -- Line 3
-            sll_3 <= to_stdlogicvector(to_bitvector(adder_4) sll 2);
-            adder_6 <= sub_1 + sll_3;
-            adder_7 <= adder_6 + sub_2;
-            --
+        sll_1 <= to_stdlogicvector(to_bitvector(adder_4) sll 4);
+        sub_1 <= sll_1 - adder_4;
+        sll_2 <= to_stdlogicvector(to_bitvector(adder_5) sll 4);
+        sub_2 <= sll_2 - adder_5;
 
-            -- Line 4
-            sll_4 <= to_stdlogicvector(to_bitvector(sub_0) sll 3);
-            adder_9 <= sll_4 + adder_7;
-            --
+        sll_3 <= to_stdlogicvector(to_bitvector(adder_4) sll 2);
 
-            srl_0 <= to_stdlogicvector(to_bitvector(adder_8) srl 14);
-            srl_1 <= to_stdlogicvector(to_bitvector(adder_8) srl 11);
-            sub_3 <= srl_1 - srl_0;
+        -- Line 3
+        adder_6 <= sub_1 + sll_3;
+        adder_7 <= adder_6 + sub_2;
+        --
+        
+        sll_4 <= to_stdlogicvector(to_bitvector(sub_0) sll 3);
 
-            -- Line 5
-            srl_2 <= to_stdlogicvector(to_bitvector(adder_8) srl 8);
-            srl_3 <= to_stdlogicvector(to_bitvector(adder_8) srl 9);
-            adder_9 <= srl_2 + srl_3;
-            adder_10 <= adder_9 + sub_3;
-            --
+        -- Line 4
+        adder_8 <= sll_4 + adder_7; -- Adder 8 is the sum result
+        --
 
-            out_val <= out_val_reg;
+        srl_0 <= to_stdlogicvector(to_bitvector(adder_8) srl 14);
+        srl_1 <= to_stdlogicvector(to_bitvector(adder_8) srl 11);
+        sub_3 <= srl_1 - srl_0;
+
+        -- Line 5
+        srl_2 <= to_stdlogicvector(to_bitvector(adder_8) srl 8);
+        srl_3 <= to_stdlogicvector(to_bitvector(adder_8) srl 9);
+        adder_9 <= srl_2 + srl_3;
+        adder_10 <= adder_9 + sub_3;
+        --
+
+        out_val <= out_val_reg;
 
             
 end architecture behavioral;
--- src/shiftAddersSubtractors/tb_gaussianFilter3x3.vhd
+++ src/shiftAddersSubtractors/tb_gaussianFilter3x3.vhd
@@ -12,20 +12,20 @@ architecture behavior of tb_gaussianFilter3x3 is
         port(
             clk     : in std_logic;
             rst     : in std_logic;
-            in_0   : in std_logic_vector(7 downto 0);
-            in_1   : in std_logic_vector(7 downto 0);
-            in_2   : in std_logic_vector(7 downto 0);
-            in_3   : in std_logic_vector(7 downto 0);
-            in_4   : in std_logic_vector(7 downto 0);
-            in_5   : in std_logic_vector(7 downto 0);
-            in_6   : in std_logic_vector(7 downto 0);
-            in_7   : in std_logic_vector(7 downto 0);
-            in_8   : in std_logic_vector(7 downto 0);
+            in_0   : in std_logic_vector(15 downto 0);
+            in_1   : in std_logic_vector(15 downto 0);
+            in_2   : in std_logic_vector(15 downto 0);
+            in_3   : in std_logic_vector(15 downto 0);
+            in_4   : in std_logic_vector(15 downto 0);
+            in_5   : in std_logic_vector(15 downto 0);
+            in_6   : in std_logic_vector(15 downto 0);
+            in_7   : in std_logic_vector(15 downto 0);
+            in_8   : in std_logic_vector(15 downto 0);
             out_val : out std_logic_vector(15 downto 0)
             );
     end component;
 
-type matrix_3x3 is array (0 to 2, 0 to 2) of std_logic_vector(7 downto 0);
+type matrix_3x3 is array (0 to 2, 0 to 2) of std_logic_vector(15 downto 0);
 signal clk_sg      : std_logic :='0';
 signal rst_sg      : std_logic := '1';
 signal in_val_sg   : matrix_3x3;
@@ -51,24 +51,47 @@ begin
     clk_sg <= not clk_sg after 5 ns;
     process
     begin
-        wait for 5 ns;
+        wait for 1 ns; -- Pouco antes do primeiro rise clock
             rst_sg <= '0';
-            in_val_sg(0, 0) <= "00000001";
-            in_val_sg(0, 1) <= "00000001";
-            in_val_sg(0, 2) <= "00000001";
-            in_val_sg(1, 0) <= "00000001";
-            in_val_sg(1, 1) <= "00000001";
-            in_val_sg(1, 2) <= "00000001";
-            in_val_sg(2, 0) <= "00000001";
-            in_val_sg(2, 1) <= "00000001";
-            in_val_sg(2, 2) <= "00000001";
-        wait for 5 ns;
-
-
-
-
-
-    -- Aguarde um tempo para que a entidade processe os valores
-    wait for 10 ns;
+            in_val_sg(0, 0) <= "0000000000000001";
+            in_val_sg(0, 1) <= "0000000000000001";
+            in_val_sg(0, 2) <= "0000000000000001";
+            in_val_sg(1, 0) <= "0000000000000001";
+            in_val_sg(1, 1) <= "0000000000000001";
+            in_val_sg(1, 2) <= "0000000000000001";
+            in_val_sg(2, 0) <= "0000000000000001";
+            in_val_sg(2, 1) <= "0000000000000001";
+            in_val_sg(2, 2) <= "0000000000000001";
+        wait for 15 ns;
+            in_val_sg(0, 0) <= "0000000000000010";
+            in_val_sg(0, 1) <= "0000000000000010";
+            in_val_sg(0, 2) <= "0000000000000010";
+            in_val_sg(1, 0) <= "0000000000000010";
+            in_val_sg(1, 1) <= "0000000000000010";
+            in_val_sg(1, 2) <= "0000000000000010";
+            in_val_sg(2, 0) <= "0000000000000010";
+            in_val_sg(2, 1) <= "0000000000000010";
+            in_val_sg(2, 2) <= "0000000000000010";
+        wait for 20 ns; -- 3 ns antes do segundo rise clock
+            in_val_sg(0, 0) <= "0000000000000011";
+            in_val_sg(0, 1) <= "0000000000000011";
+            in_val_sg(0, 2) <= "0000000000000011";
+            in_val_sg(1, 0) <= "0000000000000011";
+            in_val_sg(1, 1) <= "0000000000000011";
+            in_val_sg(1, 2) <= "0000000000000011";
+            in_val_sg(2, 0) <= "0000000000000011";
+            in_val_sg(2, 1) <= "0000000000000011";
+            in_val_sg(2, 2) <= "0000000000000011";
+        wait for 20 ns;
+            in_val_sg(0, 0) <= "0000000000000100";
+            in_val_sg(0, 1) <= "0000000000000100";
+            in_val_sg(0, 2) <= "0000000000000100";
+            in_val_sg(1, 0) <= "0000000000000100";
+            in_val_sg(1, 1) <= "0000000000000100";
+            in_val_sg(1, 2) <= "0000000000000100";
+            in_val_sg(2, 0) <= "0000000000000100";
+            in_val_sg(2, 1) <= "0000000000000100";
+            in_val_sg(2, 2) <= "0000000000000100";
+        wait;
     end process;
 end behavior;
